bebdb775dfa7bf365d2374d126339de5328061e3
AregAmirjanyan <areg.amirjanyan@gmail.com>
2024-04-10 00:41:16 +0400

    some minor changes

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
-import pathlib
-from setuptools import setup, find_packages
-
-setup(
-    name="payment_optimizer",
-    description="Optimizing the payment process of e-commerce",
-    long_description=pathlib.Path("README.md").read_text(),
-    long_description_content_type="text/markdown",
-    author="Elen Sukiasyan, Nane Mambreyan, Areg Amirjanyan, Gayane Ohanjanyan, Hasmik Sahakyan",
-    license="The Unlicense",
-    classifiers=[
-        "Development Status :: 3 - Alpha"
-    ],
-    python_requires=">=3.10, <3.12", 
-    install_requires=["requests", "pandas>=2.0"],
-    packages=find_packages(include=["Documents", "payment_optimizer", 'payment_optimizer.*', 'test', 'tests.*']),
-    version = "0.4.0"
-
+import pathlib 
+from setuptools import setup, find_packages 
+ 
+with open("requirements.txt") as f: 
+    install_requires = f.read().splitlines() 
+ 
+setup( 
+    name="payment_optimizer", 
+    description="Optimizing the payment process of e-commerce", 
+    long_description=pathlib.Path("README.md").read_text(), 
+    long_description_content_type="text/markdown", 
+    author="Elen Sukiasyan, Nane Mambreyan, Areg Amirjanyan, Gayane Ohanjanyan, Hasmik Sahakyan", 
+    license="MIT", 
+    classifiers=[ 
+        "Development Status :: 3 - Alpha" 
+    ], 
+    python_requires=">=3.10, <3.12",  
+    install_requires=install_requires, 
+    packages=find_packages(include=["Documents", "payment_optimizer", 'payment_optimizer.*', 'test', 'tests.*']), 
+    version = "0.4.0" 
+ 
 )
