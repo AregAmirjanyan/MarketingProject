434d05a83693629cff2b9bad84f30dee55938fb1
HaSahakyan <hasmik.sahakyan.y.2@gmail.com>
2024-04-25 03:58:41 +0400

    Model improvement and test changes

--- payment_optimizer/modeling/models.py
+++ payment_optimizer/modeling/models.py
@@ -2,6 +2,8 @@ import pandas as pd
 from scipy.stats import ttest_ind
 from ..db.sql_interactions import SqlHandler
 import math
+from datetime import datetime, timedelta
+
 
 
 class DatabaseConnector:
@@ -37,6 +39,11 @@ class DatabaseConnector:
 class ABTesting():
     def __init__(self, db_connector):
         self.db_connector = db_connector
+        self.first_date = None
+        self.last_date = None
+        self.t_stat = None
+        self.p_value = None
+        self.result = None
 
     def preprocess_data(self):
         
@@ -45,11 +52,41 @@ class ABTesting():
         file_path = "data.csv"
         self.db_connector.df.to_csv(file_path, index=False)
         '''
+        # Convert 'date' column to datetime type
+        data['date'] = pd.to_datetime(data['date'])
 
-        self.control_group = data[data['explored_bandit_type'] == 'bandit A']
-        self.treatment_group = data[data['explored_bandit_type'] == 'bandit B']
+        self.first_date = data['date'].min()
+        self.last_date = data['date'].max()
+        self.control_group = data[(data['explored_bandit_type'] == 'bandit A') & (data['status'] == 'purchased')]
+        self.treatment_group = data[(data['explored_bandit_type'] == 'bandit B') & (data['status'] == 'purchased')]
+        
+    def parse_date(self, date_str):
+        try:
+            return datetime.strptime(date_str, '%d/%m/%Y')
+        except ValueError:
+            print("Invalid date format. Please use 'dd/mm/yyyy'.")
+            return None
     
-    def perform_ab_test(self):
+    def perform_ab_test(self, start_date = None, end_date = None):
+        #start_date and end_date should be in format dd/mm/yyyy
+
+        # Filter data by start_date and end_date if provided
+        if start_date:
+            self.first_date = self.parse_date(start_date)
+            if self.first_date is None:
+                return
+            self.control_group = self.control_group[self.control_group['date'] >= self.first_date]
+            self.treatment_group = self.treatment_group[self.treatment_group['date'] >= self.first_date]
+        
+        if end_date:
+            self.last_date = self.parse_date(end_date)
+            if self.last_date is None:
+                return
+            # Increment the end_date by one day to include the end_date itself
+            self.last_date += timedelta(days=1)
+            self.control_group = self.control_group[self.control_group['date'] <= self.last_date]
+            self.treatment_group = self.treatment_group[self.treatment_group['date'] <= self.last_date]
+
         #Choosing Average Order Value (AOV) metrics for control and treatment groups
         control_metric = self.control_group['price'] * self.control_group['quantity'] * math.log(len(self.control_group))  # AOV for control group
         treatment_metric = self.treatment_group['price'] * self.treatment_group['quantity'] * math.log(len(self.treatment_group))  # AOV for treatment group
@@ -66,10 +103,13 @@ class ABTesting():
     
         
     def results(self):
-        ab_test_results = {'Model': "A/B Testing",
-                                        't_stat': self.t_stat,
-                                        'p_value': self.p_value}
-        #ab_test_results.to_sql('ab_test_results', self.conn, if_exists='replace', index=False)
-        print(self.result)
+        ab_test_results = {
+            'Start Date': self.first_date,
+            'End Date': self.last_date,
+            't_stat': self.t_stat,
+            'p_value': self.p_value,
+            'Message': self.result
+        }
+        #print(self.result)
         return ab_test_results
 
--- tests/test_modeling.py
+++ tests/test_modeling.py
@@ -14,10 +14,39 @@ if __name__ == "__main__":
     print('---------------Testing A/B test')
 
     # Initialize ABTesting instance with joined datasets
-    ab_test = ABTesting(data_connect)
-    ab_test.preprocess_data()
-    ab_test.perform_ab_test()
-    res = ab_test.results()
-    print(res)
+    ab_test_1 = ABTesting(data_connect)
+    ab_test_1.preprocess_data()
+    ab_test_1.perform_ab_test()
+    res_1 = ab_test_1.results()
+    #print(res_1)
+
+
+    print('---------------Testing A/B test with start date specifications')
+
+    # Initialize ABTesting instance with joined datasets
+    ab_test_2 = ABTesting(data_connect)
+    ab_test_2.preprocess_data()
+    ab_test_2.perform_ab_test("04/07/2022")
+    res_2 = ab_test_2.results()
+    #print(res_2)
+
+    print('---------------Testing A/B test with start and end date specifications ')
+
+    # Initialize ABTesting instance with joined datasets
+    ab_test_3 = ABTesting(data_connect)
+    ab_test_3.preprocess_data()
+    ab_test_3.perform_ab_test("04/02/2022","30/04/2023")
+    res_3 = ab_test_3.results()
+    #print(res_3)
+
+    # Create a DataFrame from the results
+    a_b_testing_results = pd.DataFrame([res_1, res_2, res_3])
+    print(a_b_testing_results)
+
+
+    #results_to_db = SqlHandler('e_commerce', 'a_b_testing_results')
+
+    #results_to_db.insert_many(a_b_testing_results)
+    #results_to_db.close_cnxn()
 
     
